0c9e8881ba89a8198570532bc65e5286339da1a0
Nick Kasten <kastentx@gmail.com>
2016-06-06 00:19:02 +0000

    connecting to db via Mongoose

--- urlcheck.js
+++ urlcheck.js
@@ -1,6 +1,6 @@
 var exports = module.exports = {}
 
-var url = process.env.MONGOLAB_URI
+var dbUrl = process.env.MONGOLAB_URI
 var mongoose = require('mongoose')
 var URL = mongoose.model('URL', {original: String, shortened: String})
 
@@ -11,8 +11,8 @@ exports.checkValidity = function(myInput) {
 }
 
 var insertTest = function(callback) {
-  mongoose.connect(url)
-  
+  mongoose.connect(dbUrl)
+  /*
   var test1 = new URL({original: 'www.google.com', shortened: 'www.goog.com'})
   
   test1.save(function (err, testObj) {
@@ -22,5 +22,12 @@ var insertTest = function(callback) {
       console.log('successfully saved ' + testObj)
     }
   })
+  */
+  var db = mongoose.connection
   
+  db.on('error', console.error.bind(console, 'connection error:'))
+  db.once('open', function() {
+    console.log("Connected to DB")
+  //do operations which involve interacting with DB.
+  })
 }
